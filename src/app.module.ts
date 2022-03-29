a2a84b1b7e4df0d50ebf9daf121697da9f23daf6
Hoslack Ochieng <hoslackochieng@gmail.com>
2022-03-29 08:35:00 +0300

    refactor(appmodule): remove test controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LocksModule } from './locks/locks.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UsersModule, LocksModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
